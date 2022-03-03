0b1139a602a8051ba3a9efc5d529badc0ea5bafe
Tino Adams <tino.a77@gmail.com>
2022-03-03 23:59:57 +1100

    transparent iframes

--- src/components/_dashboard/app/Grafana.js
+++ src/components/_dashboard/app/Grafana.js
@@ -5,7 +5,7 @@ export default function Grafana(props) {
   if (props.expand === false) return null;
   return (
     <div>
-      <embed title="panel" src={props.url} width="100%" height="200px" frameBorder="0" />
+      <iframe title="panel" src={props.url} width="100%" height="200px" frameBorder="0" allowtransparency="true" />
     </div>
   );
 }
